d3a91a1dcc79022ce8f164e528e8217224016b87
Ashwani Pandey <ashwani.pandey@eclinicalworks.com>
2019-10-04 09:45:43 +0530

    Add support for sacred logging of ML experiments.

--- main_sdd.py
+++ main_sdd.py
@@ -1,18 +1,46 @@
 import os
 import time
-import random
 from pathlib import Path
-import numpy as np
+
 import pandas as pd
-from sklearn.model_selection import train_test_split
-# import tensorflow as tf
 from keras import callbacks, utils, models
+from sacred import Experiment
+from sacred.observers import MongoObserver
+from sacred.utils import apply_backspaces_and_linefeeds
+from sklearn.model_selection import train_test_split
 
-from utils.data_utils import DataSequence
 from models_sdd import SDDModel
+from utils.data_utils import DataSequence
+
+_CUDA_VISIBLE_DEVICES = "3"
+_MODEL_ARC = 'deeplab'
+os.environ["CUDA_VISIBLE_DEVICES"] = _CUDA_VISIBLE_DEVICES
+os.environ["TF_FORCE_GPU_ALLOW_GROWTH"] = "true"
+
+ex = Experiment(name=_MODEL_ARC)
+ex.observers.append(MongoObserver.create(
+    url='127.0.0.1:27017', db_name='severstal_sdd'))
+ex.captured_out_filter = apply_backspaces_and_linefeeds
 
 
-def run(backbone, batch_size, lr, dropout_rate, data_path, artifacts_folder, img_size, use_multi_gpu):
+# noinspection PyUnusedLocal
+@ex.config()
+def config():
+    seed = 42
+    gpu_count = len(_CUDA_VISIBLE_DEVICES.split(','))
+    backbone = 'resnet18',
+    batch_size = 4,
+    lr = 0.0001
+    dropout_rate = 0.2
+    data_path = 'data'
+    artifacts_folder = f'artifacts/{_MODEL_ARC}/{time.strftime("%d-%m-%y_%H:%M", time.localtime())}',
+    img_size = (256, 1600, 3),
+    use_multi_gpu = gpu_count > 1
+
+
+@ex.automain()
+def run(backbone, batch_size, lr, dropout_rate, data_path, artifacts_folder,
+        img_size, use_multi_gpu, gpu_count, seed, _run):
     artifacts_folder = Path(artifacts_folder)
     artifacts_folder.mkdir(parents=True, exist_ok=True)
     data_path = Path(data_path)
@@ -33,17 +61,17 @@ def run(backbone, batch_size, lr, dropout_rate, data_path, artifacts_folder, img
     print(train_df.info())
     print(train_df.head(10))
 
-    train_df, val_df = train_test_split(train_df, test_size=0.2, random_state=_SEED)
+    train_df, val_df = train_test_split(train_df, test_size=0.2, random_state=seed)
     print(f'length of train and val data: {len(train_df.index)}, {len(val_df.index)}')
 
     ckpt_path = artifacts_folder / 'ckpts'
     ckpt_path.mkdir(exist_ok=True, parents=True)
     parallel_model = None
     if use_multi_gpu:
-        sdd_model, parallel_model = SDDModel(backbone, img_size, lr, dropout_rate, model_arc,
+        sdd_model, parallel_model = SDDModel(backbone, img_size, lr, dropout_rate, _MODEL_ARC,
                                              use_multi_gpu=use_multi_gpu, gpu_count=gpu_count).get_model()
     else:
-        sdd_model = SDDModel(backbone, img_size, lr, dropout_rate, model_arc).get_model()
+        sdd_model = SDDModel(backbone, img_size, lr, dropout_rate, _MODEL_ARC).get_model()
     sdd_model.summary()
     utils.plot_model(sdd_model, str(artifacts_folder / 'model.png'), show_shapes=True)
     training_callbacks = [
@@ -55,8 +83,8 @@ def run(backbone, batch_size, lr, dropout_rate, data_path, artifacts_folder, img
         callbacks.TerminateOnNaN()
     ]
 
-    train_seq = DataSequence(_SEED, train_df, batch_size, img_size, 'data/train_images', shuffle=True, augment=True)
-    val_seq = DataSequence(_SEED, val_df, batch_size, img_size, 'data/train_images', shuffle=False, augment=False)
+    train_seq = DataSequence(seed, train_df, batch_size, img_size, 'data/train_images', shuffle=True, augment=True)
+    val_seq = DataSequence(seed, val_df, batch_size, img_size, 'data/train_images', shuffle=False, augment=False)
     if use_multi_gpu:
         training_model = parallel_model
     else:
@@ -66,31 +94,8 @@ def run(backbone, batch_size, lr, dropout_rate, data_path, artifacts_folder, img
                                            use_multiprocessing=True)
     models.save_model(sdd_model, str(artifacts_folder / 'model_best.h5'))
 
-    pd.DataFrame(history.history).to_csv(artifacts_folder / 'history.csv', index=False)
+    pd.DataFrame(history.history).to_csv('history.csv', index=False)
+    _run.add_artifact('history.csv')
+    os.remove('history.csv')
 
-
-if __name__ == '__main__':
-    start_time = time.time()
-    cuda_visible_devices = "3"
-    os.environ["CUDA_VISIBLE_DEVICES"] = cuda_visible_devices
-    os.environ["TF_FORCE_GPU_ALLOW_GROWTH"] = "true"
-
-    _SEED = 42
-    random.seed(_SEED)
-    np.random.seed(_SEED)
-    # tf.compat.v1.random.set_random_seed(_SEED)
-
-    gpu_count = len(cuda_visible_devices.split(','))
-    model_arc = 'deeplab'
-    params = {
-        'backbone': 'resnet18',
-        'batch_size': 4,
-        'lr': 0.0001,
-        'dropout_rate': 0.2,
-        'data_path': 'data',
-        'artifacts_folder': f'artifacts/{model_arc}/{time.strftime("%d-%m-%y_%H:%M", time.localtime())}',
-        'img_size': (256, 1600, 3),
-        'use_multi_gpu': gpu_count > 1
-    }
-    run(**params)
-    print(f'Total run time: {(time.time() - start_time) / 60} minutes.')
+    return history.history['val_loss'][-1]
